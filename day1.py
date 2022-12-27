505f494f86b2c2db450d79be421b4b78ab7b348e
Priyanka-102001 <87640810+Priyanka-102001@users.noreply.github.com>
2022-12-27 11:25:43 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+print("enter the character")
+ch=input()
+if(ch=='A'or ch=='E'or ch=='U'or ch=='O'or ch=='I'or ch=='a'or ch=='e'or ch=='u'or ch=='o'or ch=='i'):
+ print("volwel")
+elif((ch>='A'and ch<='Z')or(ch>='a'and ch>='z')):
+ print("constant")
+else:
+ print("invalid input")
+    
